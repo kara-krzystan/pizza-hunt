00a6ecda990a51164ef9536e0174f57a999b6c93
Kara Krzystan <kara.krzystan@gmail.com>
2021-02-12 13:59:49 -0700

    Create the pizza api routes

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const pizzaController = require('../../controllers/pizza-controller');
+const pizzaRoutes = require('./pizza-routes');
+
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
