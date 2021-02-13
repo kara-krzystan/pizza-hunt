f57ec25b3cd9f5c95c97ff78bdbfcc50c35139c5
Kara Krzystan <kara.krzystan@gmail.com>
2021-02-12 19:35:01 -0700

    Added and tested offline persistence

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -1,5 +1,4 @@
 let db;
-
 const request = indexedDB.open('pizza_hunt', 1);
 
 request.onupgradeneeded = function(event) {
@@ -11,11 +10,55 @@ request.onsuccess = function(event) {
   db = event.target.result;
 
   if (navigator.onLine) {
-    // uploadPizza();
+    uploadPizza();
   }
 };
 
 request.onerror = function(event) {
-  // log error here
   console.log(event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+         
+          pizzaObjectStore.clear();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadPizza);
